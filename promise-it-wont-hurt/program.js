58ec07971cae9f5c3f1845f05c14a3fc43c08e27
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-05-25 11:14:23 -0400

    Promise-it-wont-hurt lesson 2

--- promise-it-wont-hurt/program.js
+++ promise-it-wont-hurt/program.js
@@ -1,3 +1,13 @@
-setTimeout(function(){
+require('es6-promise');
+
+var promise = new Promise(function (fulfill, reject) {
+    setTimeout(function() {
+        fulfill("FULFILLED!")
+    }, 300);
+});
+
+promise.then(console.log);
+
+/*setTimeout(function(){
     console.log("TIMED OUT!");
-}, 300);
+}, 300);*/
