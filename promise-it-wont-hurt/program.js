4ebdf139f97c8df768aeceb0939f655856e365d1
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-05-25 16:56:44 -0400

    Promise-it-wont-hurt 7

--- promise-it-wont-hurt/program.js
+++ promise-it-wont-hurt/program.js
@@ -1,20 +1,18 @@
 require('es6-promise');
 
-var promise = new Promise(function (fulfill, reject) {
+/*var promise = new Promise(function (fulfill, reject) {
     console.log("Printing things yo")
-});
+});*/
 
-promise = Promise.reject(new Error("UH OH"));
+
+
+first().then(function(value) {
+    second(value).then(console.log);}
+)
+
 
 /*function onReject(error) {
     console.log(error.message);
 }*/
 
-promise.catch(function() { console.log(error.message)})
-
-//promise.then(console.log, onReject);
-console.log("MAIN PROGRAM")
-
-/*setTimeout(function(){
-    console.log("TIMED OUT!");
-}, 300);*/
+//promise.catch(function() { console.log(error.message)})
